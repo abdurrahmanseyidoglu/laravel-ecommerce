80fc621e59726b50ce267243d6d8b43dc2c88d74
Abdurrahman <abdurrahmanseyidoglu@gmail.com>
2022-05-25 14:44:42 +0300

    adding taxes and total price to order page

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -12,10 +12,12 @@ $cartItems = ProductController::cartItem() //using a function inside ProductCont
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <meta charset="UTF-8">
     <!-- Font Awesome -->
+    <!-- Font Awesome -->
     <link
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
         rel="stylesheet"
     />
+
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.0/css/toastr.css" rel="stylesheet"/>
 
     <!-- Google Fonts -->
--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,5 +1,5 @@
 @include('header')
-<section class="h-100 h-custom mt-5" >
+<section class="h-100 h-custom mt-5">
     <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-lg-8 col-xl-6">
@@ -11,36 +11,40 @@
                         <div class="row">
                             <div class="col mb-3">
                                 <p class="small text-muted mb-1">Date</p>
-                                <p>10 April 2021</p>
+                                <p>@php
+                                        $now = date('Y-m-d');
+                                         echo $now;
+                                    @endphp</p>
                             </div>
                             <div class="col mb-3">
-                                <p class="small text-muted mb-1">Price</p>
-                                <p>012j1gvs356c</p>
+                                <p class="small text-muted mb-1">Items Price</p>
+                                <p class="h5">{{$total}}$</p>
                             </div>
                         </div>
 
                         <div class="mx-n5 px-5 py-4" style="background-color: #f2f2f2;">
                             <div class="row">
                                 <div class="col-md-8 col-lg-9">
-                                    <p>BEATS Solo 3 Wireless Headphones</p>
+                                    <p>Total of Items price</p>
                                 </div>
                                 <div class="col-md-4 col-lg-3">
-                                    <p>£299.99</p>
+                                    <p class="h5">{{$total}}$</p>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-8 col-lg-9">
-                                    <p class="mb-0">Shipping</p>
+                                    <p class="mb-0">Taxes (8%) </p>
                                 </div>
                                 <div class="col-md-4 col-lg-3">
-                                    <p class="mb-0">£33.00</p>
+                                    <p class="mb-0">{{$total*8/100}}$</p>
                                 </div>
                             </div>
                         </div>
 
                         <div class="row my-4">
-                            <div class="col-md-4 offset-md-8 col-lg-3 offset-lg-9">
-                                <p class="lead fw-bold mb-0" style="color: #f37a27;">£262.99</p>
+                            <div class="col-md-4">
+                                <p class=" mb-5 h2" style="color: #f37a27;">Total</p>
+                                <p class="lead fw-bold mb-0" style="color: #f37a27;">{{$total + ($total*8/100)}}$</p>
                             </div>
                         </div>
 
@@ -53,15 +57,18 @@
 
                                     <ul class="list-inline items d-flex justify-content-between">
                                         <li class="list-inline-item items-list">
-                                            <p class="py-1 px-2 rounded text-white" style="background-color: #f37a27;">Ordered</p
+                                            <p class="py-1 px-2 rounded text-white" style="background-color: #f37a27;">
+                                                Ordered</p
                                                 class="py-1 px-2 rounded text-white" style="background-color: #f37a27;">
                                         </li>
                                         <li class="list-inline-item items-list">
-                                            <p class="py-1 px-2 rounded text-white" style="background-color: #f37a27;">Shipped</p
+                                            <p class="py-1 px-2 rounded text-white" style="background-color: #f37a27;">
+                                                Shipped</p
                                                 class="py-1 px-2 rounded text-white" style="background-color: #f37a27;">
                                         </li>
                                         <li class="list-inline-item items-list">
-                                            <p class="py-1 px-2 rounded text-white" style="background-color: #f37a27;">On the way
+                                            <p class="py-1 px-2 rounded text-white" style="background-color: #f37a27;">
+                                                On the way
                                             </p>
                                         </li>
                                         <li class="list-inline-item items-list text-end" style="margin-right: 8px;">
@@ -74,7 +81,8 @@
                             </div>
                         </div>
 
-                        <p class="mt-4 pt-2 mb-0">Want any help? <a href="#!" style="color: #f37a27;">Please contact
+                        <p class="mt-4 pt-2 mb-0">Want any help? <a href="contact" style="color: #f37a27;">Please
+                                contact
                                 us</a></p>
 
                     </div>
@@ -83,4 +91,35 @@
         </div>
     </div>
 </section>
+<style>
+    @media (min-width: 1025px) {
+        .h-custom {
+            height: 100vh !important;
+        }
+    }
+
+    .horizontal-timeline .items {
+        border-top: 2px solid #ddd;
+    }
+
+    .horizontal-timeline .items .items-list {
+        position: relative;
+        margin-right: 0;
+    }
+
+    .horizontal-timeline .items .items-list:before {
+        content: "";
+        position: absolute;
+        height: 8px;
+        width: 8px;
+        border-radius: 50%;
+        background-color: #ddd;
+        top: 0;
+        margin-top: -5px;
+    }
+
+    .horizontal-timeline .items .items-list {
+        padding-top: 15px;
+    }
+</style>
 @include('footer')

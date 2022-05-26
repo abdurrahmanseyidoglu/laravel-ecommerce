41d563a6f8c2ec1b5386d703cb3b208ab939b084
Abdurrahman <abdurrahmanseyidoglu@gmail.com>
2022-05-26 10:04:05 +0300

    fixing some styles

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="products">
+                    <a href="/products">
                         Home
                     </a>
                 </div>
